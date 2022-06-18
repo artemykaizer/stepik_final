244ee0f208ea4e8ae9be7417ae7c52228ba3f7b8
artemy <artemy.kaizer@yandex.ru>
2022-06-18 16:21:35 +0600

    added page switch

--- pages/login_page.py
+++ pages/login_page.py
@@ -12,9 +12,7 @@ class LoginPage(BasePage):
         assert "login" in current_url, "URL is not correct"
 
     def should_be_login_form(self):
-        login_form = self.browser.find_element(*LoginPageLocators.LOGIN_FORM)
-        assert self.is_element_present(login_form), "Login form is not present"
+        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Login form is not present"
 
     def should_be_register_form(self):
-        register_form = self.browser.find_element(*LoginPageLocators.REGISTRATION_FORM)
-        assert self.is_element_present(register_form), "Register form is not present"
+        assert self.is_element_present(*LoginPageLocators.REGISTRATION_FORM), "Register form is not present"
