249d49d450d7cec2b5fa35c2e3d01f85bff9bbc8
artemy <artemy.kaizer@yandex.ru>
2022-06-19 19:19:53 +0600

    refactor login tests

--- pages/locators.py
+++ pages/locators.py
@@ -16,4 +16,9 @@ class ProductPageLocators():
     PRODUCT_NAME = (By.CSS_SELECTOR, "div.product_main h1")
     PRICE_TOTAL = (By.CSS_SELECTOR, ".alert-info .alertinner strong")
     PRICE_OF_THE_PRODUCT = (By.CSS_SELECTOR, "p.price_color")
+    SUCCESS_MESSAGE = (By.CSS_SELECTOR, ".alert.alert-success")
 
+
+class BasePageLocators():
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
--- pages/product_page.py
+++ pages/product_page.py
@@ -16,3 +16,9 @@ class ProductPage(BasePage):
         price_total = self.browser.find_element(*ProductPageLocators.PRICE_TOTAL).text
         price_of_the_book = self.browser.find_element(*ProductPageLocators.PRICE_OF_THE_PRODUCT).text
         assert price_of_the_book in price_total, "Incorrect price"
+
+    def should_not_be_success_message(self):
+        assert self.is_not_element_present(*ProductPageLocators.SUCCESS_MESSAGE), "Присутствует сообщение об успехе"
+
+    def element_should_disappear(self):
+        assert self.is_disappeared(*ProductPageLocators.SUCCESS_MESSAGE), "Элемент не исчез"
